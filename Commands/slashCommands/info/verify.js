c05f2f19e22518962b8faca3d461fb8bd3107c08
Connor <82512292+connorsc1@users.noreply.github.com>
2022-08-20 18:20:15 +0000

    .

--- Commands/slashCommands/info/verify.js
+++ Commands/slashCommands/info/verify.js
@@ -1,5 +1,5 @@
 const { Modal, TextInputComponent, SelectMenuComponent, showModal } = require('discord-modals');
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandType, ButtonStyle } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandType, ButtonStyle, InteractionType } = require('discord.js');
 module.exports = {
 	name: 'verify',
 	description: "Allows users to verify their accounts to gain a role.",
@@ -11,30 +11,66 @@ module.exports = {
 					.setLabel('Verify')
 					.setStyle(ButtonStyle.Success),
 			);
-
-    const embed = new EmbedBuilder()
-		.setTitle(`.`)
-		.setColor('#2F3136')
+    
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 6; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
     
     const modal = new Modal()
     	.setCustomId('verify')
-    	.setTitle('Verification')
+    	.setTitle(`Verification Code = ${text}`)
     	.addComponents(
     		new TextInputComponent()
     			.setCustomId('verificationinput')
     			.setLabel('Verify')
     			.setStyle('SHORT')
-    			.setPlaceholder('Write "Verify" here')
+    			.setPlaceholder(`Write ${text} here`)
     			.setRequired(true),
     	);
+    
     client.on('interactionCreate', interaction => {
       if (interaction.customId === 'button1') {
-        interaction.showModal(modal, {
+        return interaction.showModal(modal, {
 			    client: client,
 			    interaction: interaction,
 		    });
   	  }
     });
-    interaction.reply({ components: [row], ephemeral: true});
+
+    client.on('interactionCreate', interaction => {
+	    if (interaction.type !== InteractionType.ModalSubmit) return;
+        const code = interaction.fields.getTextInputValue('verificationinput');
+        if (code != text) {
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setColor("#2F3136")
+                .setDescription("Incorrect Code, please try again"),
+            ],
+            ephemeral: true,
+            components: [row]
+          });
+        }
+        
+        if (code == text) {
+
+          const role = interaction.guild.roles.cache.get("1010501304973529158");
+          interaction.member.roles.add(role)
+          interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setColor("#2F3136")
+                .setDescription("Verified"),
+            ],
+            ephemeral: true
+          });
+        }
+      
+    });
+    
+    await interaction.reply({ components: [row], ephemeral: true});
   }
 };
